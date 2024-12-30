fb9d218ee629a3e65749f6172bc91a309c3e036f
panji881 <evaprassaputra@gmail.com>
2024-12-31 01:09:35 +0700

    tugas pertemuan 7

---
+++ database/migrations/2024_11_26_011640_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) { 
+            $table->id(); 
+            $table->string('nama'); 
+            $table->string('email')->unique(); 
+            $table->enum('role',[0,1,2])->default(0); // 0= Admin, 1= SuperAdmin, 2= Customer  
+            $table->boolean('status'); //0= Belum Aktif, 1= Aktif 
+            //$table->timestamp('email_verified_at')->nullable(); 
+            $table->string('password'); 
+            $table->string('hp', 13); 
+            //$table->rememberToken(); 
+            $table->string('foto')->nullable(); 
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
